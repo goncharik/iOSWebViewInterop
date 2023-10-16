c4419498bd05db7d4171444ac3c24207e192e38b
Eugene Honcharenko <tulusheg@gmail.com>
2023-10-16 04:51:51 -0400

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Eugene Honcharenko on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
